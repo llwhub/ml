a78718e67c86f5d7987f61c4d991b07fb77a36ab
lifuqing <lifq19@ziroom.com>
2019-08-27 15:54:58 +0800

    KMeansJava实现

---
+++ src/main/java/com/lifq/demo/KMeanCluster.java
@@ -0,0 +1,34 @@
+package com.lifq.demo;
+
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.tools.data.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * <pre>
+ * desc ：聚类Kmeans
+ * author ：lfq
+ * date ：2019-08-27 14:22
+ * </pre>
+ */
+
+public class KMeanCluster {
+    public static void main(String[] args) throws IOException {
+        // 加载数据
+        Dataset dataSet = FileHandler.loadDataset(new File("src/main/resources/zscoreddata.csv"),-1,",");
+        //分成5类
+        KMeans km = new KMeans(5);
+        //分类数据
+        Dataset[] clusters  = km.cluster(dataSet);
+        System.out.println(clusters.length);
+        for(Dataset res:clusters){
+            for(Object obj:res.classes()){
+                System.out.println(obj.toString());
+            }
+        }
+    }
+}
